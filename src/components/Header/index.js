93e7127fa377d3efc1e79b6bf9b170f4df03fdb5
Ivens Diego Müller <ivens.muller@amazingit.com.br>
2020-04-08 18:03:41 -0300

    Upload user avatar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '~/assets/logo-purple.svg';
@@ -8,6 +9,8 @@ import Notifications from '~/components/Notifications';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -21,11 +24,14 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Ivens</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="perfil"
             />
           </Profile>
